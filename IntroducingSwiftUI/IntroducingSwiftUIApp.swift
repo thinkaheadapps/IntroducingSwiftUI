8bb268e7f4d2fffd685d58f9871c17fa3606495f
Divya Rai <thinkaheadapps@gmail.com>
2024-01-11 11:01:22 +0530

    Initial Commit

---
+++ IntroducingSwiftUI/IntroducingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroducingSwiftUIApp.swift
+//  IntroducingSwiftUI
+//
+//  Created by Divya Rai on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct IntroducingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
